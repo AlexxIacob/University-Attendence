13d5b89680db41746d1b282c925b96690707e919
AlexxIacob <alexandruiacob01@gmail.com>
2025-08-04 04:21:23 +0300

    Project initialization

---
+++ backend/src/main/java/com/univ_attendence/University_Attendence/UniversityAttendenceApplication.java
@@ -0,0 +1,13 @@
+package com.univ_attendence.University_Attendence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAttendenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityAttendenceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/univ_attendence/University_Attendence/UniversityAttendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ_attendence.University_Attendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAttendenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
